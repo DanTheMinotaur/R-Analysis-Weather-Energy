ded656f1e2e3216045c92807ba37791fabe74801
Daniel <x15004091@student.ncirl.ie>
2019-04-11 16:53:00 +0100

    DataScraper and Energy Data

--- data_collection/grab_energy_data.py
+++ data_collection/grab_energy_data.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 
 # http://smartgriddashboard.eirgrid.com/DashboardService.svc/data?area=demandactual&region=ALL&datefrom=01-Apr-2019+00%3A00&dateto=09-Apr-2019+23%3A59&_=1554939751279
 
@@ -17,21 +18,34 @@ days_of_month = {
     "Dec": 31
 }
 
+def write_csv(rows):
+    with open('energy_data.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in rows:
+            writer.writerow(row)
+
 def collect_data(start, end):
     url = "http://smartgriddashboard.eirgrid.com/DashboardService.svc/data?area=demandactual&region=ALL&datefrom={start}+00%3A00&dateto={end}+23%3A59&_=1554939751279"
     return requests.get(url.format(start=start, end=end)).json()
 
 def format_data(json_data):
     data = []
-    if "ErrorMessage" in json_data and json_data["ErrorMessage"] is not None:
+    if "Rows" in json_data:
+        print(json_data)
         for data_row in json_data["Rows"]:
-            data.append(
+            data.append([
                 data_row["EffectiveTime"],
                 data_row["FieldName"],
                 data_row["Region"],
                 data_row["Value"],
-            )
-    else:
-        print(json_data)
-        print("Something went wrong with the response")
-        exit()
+            ])
+    return data
+
+for year in range(2019, 2020):
+    formatted_data = list()
+    print(year)
+    for month, last_day in days_of_month.items():
+        month_year = "{}-{}".format(month, year)
+        from_date = "01-{}".format(month_year)
+        to_date = "{}-{}".format(last_day, month_year)
+        write_csv(format_data(collect_data(from_date, to_date)))
