861bfc443c9b7d3f0fec5a9b1fcd3140155db130
Daniel <x15004091@student.ncirl.ir>
2019-04-12 23:52:05 +0100

    Removal of irrelevant data, converted to SQLite

--- Energy/clean_data.R
+++ Energy/clean_data.R
@@ -4,15 +4,25 @@
 # Created on: 12/04/2019
 
 # Install Dependencies
-install.packages("tidyr")
-install.packages(c("dbplyr", "RSQLite"))
+# install.packages("tidyr")
+# install.packages(c("dbplyr", "RSQLite"))
+# install.packages("DBI")
+# Load dependencies
+Packages <- c(
+  "tidyr",
+  "dbplyr",
+  "RSQLite",
+  "DBI"
+)
+
+lapply(Packages, library, character.only = TRUE)
 
 # Function to remove any rows with empty values 
 removeEmptyRows <- function(dataFrame) {
   # Count Total ROws in file
   total_rows <- nrow(dataFrame)
   cat("Total Rows:", total_rows, "\n")
-  clearedDataFrame <- na.omit(raw_energy)
+  clearedDataFrame <- na.omit(dataFrame)
   # Count New Total ROws in file
   total_new_rows <- nrow(clearedDataFrame)
   cat("Total Rows Left:", total_new_rows)
@@ -36,12 +46,27 @@ class(energy_removed_empty$datetime)
 # Convert string datetime into datetime object
 #energy_removed_empty$datetime <- strptime(energy_removed_empty$datetime, format="%d/%m/%Y  %H:%M:%S")
 
-strptime(energy_removed_empty$datetime, format="%d/%m/%Y  %H:%M:%S")
+# Convert to time object
+energy_removed_empty$datetime <- strptime(energy_removed_empty$datetime, format="%d-%b-%Y %T")
 
 # Check datetime datatype
 class(energy_removed_empty$datetime)
 
+#Remove useless columns
 energy_removed_empty$demandtype = NULL
+energy_removed_empty$usage = NULL
 
-energy_removed_empty
-energy_db <- src_sq
+# Check data
+head(energy_removed_empty)
+tail(energy_removed_empty)
+
+# Check datetime datatype
+class(energy_removed_empty$datetime)
+
+# Create SQLite Database
+energyDB <- dbConnect(RSQLite::SQLite(), "energy_data.sqlite")
+
+#Reformat datetimeobject to standard ISO String for SQLiteDatabase
+energy_removed_empty$datetime <- as.POSIXct(energy_removed_empty$datetime)
+
+dbWriteTable(energyDB, "irish_energy_data", energy_removed_empty)
