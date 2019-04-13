ffd0bb34b7fc6bfacef15d686be5d2444337dc21
Daniel <x15004091@student.ncirl.ir>
2019-04-13 16:08:58 +0100

    Split into different scripts

--- Energy/year_energy.R
+++ Energy/year_energy.R
@@ -8,28 +8,26 @@
 source("functions.R")
 
 Packages <- c(
-    "DBI",
     "tidyverse"
 )
 
+# Install packages
 check.packages(Packages)
 
+# Load energy data from DB
 con <- load.energydb()
-
 energy_data <- dbReadTable(con, "irish_energy_data")
-names(energy_data)
-energy_data$datetime <- lubridate::as_datetime(energy_data$datetime)
 
+# Average Energy usage
 mean(energy_data$mwh)
+
+# Groupd Energy data usage by year and average/mean values
 energy_data_by_year <- group_by(energy_data, year)
-
-tail(energy_data_by_year)
-
 year_on_year_energy_usage <- summarize(energy_data_by_year, mean_energy_usage = round(mean(mwh, na.rm = TRUE), 2))
 colnames(year_on_year_energy_usage) <- c("Year", "Mwh")
-class(year_on_year_energy_usage)
 year_on_year_energy_usage
 
+# Create Bar Plot with data
 yearly_energy_usage_bar_plot <- ggplot(
   data=year_on_year_energy_usage, 
   aes(
