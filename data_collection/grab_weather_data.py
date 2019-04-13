f6018d8d5f7c6d50e4f4d6a7e125126182b27942
Daniel <x15004091@student.ncirl.ir>
2019-04-14 00:27:42 +0100

    Added Weather Data

--- data_collection/grab_weather_data.py
+++ data_collection/grab_weather_data.py
@@ -1,2 +1,24 @@
 import bs4
 import requests
+
+weather_stations = ["Athenry","Ballyhaise","Belmullet","Casement Aerodrome","Claremorris","Cork Airport","Dublin Airport","Dunsany (Grange)","Finner Camp","Gurteen","Johnstown Castle","Knock Airport","Mace Head","Malin Head","Markree Castle","Moore Park","Mount Dillon","Mullingar","Newport Furnace","Oak Park","Phoenix Park","Roches point SWS.","Shannon Airport","Sherkin Island","Valentia Observatory"]
+
+# https://www.met.ie/api/stations/Athenry/monthly
+weather_data_url = "https://www.met.ie/api/stations/{}/monthly"
+
+
+def get_station_data(station_name):
+    response = requests.get(weather_data_url.format("Athenry")).json()
+    return response["report"]
+
+
+def parse_table(table_string):
+    soup = bs4.BeautifulSoup(table_string, features="html.parser")
+
+    tables = soup.find_all('table')
+    headers = soup.find_all('h3')
+    print(len(tables))
+    print(len(headers))
+
+
+parse_table(get_station_data(""))
