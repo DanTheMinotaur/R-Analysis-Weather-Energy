86dfaba7a1b5d60a498a2713af4b2606d636930b
Daniel <x15004091@student.ncirl.ir>
2019-04-13 15:29:54 +0100

    Visualise Average Usage

--- Energy/analyse_energy_data.R
+++ Energy/analyse_energy_data.R
@@ -22,4 +22,28 @@ con <- dbConnect(RSQLite::SQLite(), "energy_data.sqlite")
 energy_data <- dbReadTable(con, "irish_energy_data")
 names(energy_data)
 energy_data$datetime <- lubridate::as_datetime(energy_data$datetime)
-energy_data
+
+mean(energy_data$mwh)
+energy_data_by_year <- group_by(energy_data, year)
+
+tail(energy_data_by_year)
+
+summarize(energy_data, mean_energy_usage = mean(mwh, na.rm = TRUE))
+year_on_year_energy_usage <- summarize(energy_data_by_year, mean_energy_usage = round(mean(mwh, na.rm = TRUE), 2))
+colnames(year_on_year_energy_usage) <- c("Year", "Mwh")
+class(year_on_year_energy_usage)
+year_on_year_energy_usage
+
+ggplot(
+  data=year_on_year_energy_usage, 
+  aes(
+    x=Year,
+    y=Mwh,
+    fill=Year
+  )) +
+    geom_bar(stat="identity")+
+    geom_text(aes(label=Mwh, vjust=-0.2))+
+    theme_minimal()
+
+
+#barplot(year_on_year_energy_usage[,1], main="Average Energy Usage", xlab="Year", ylab="Mwh", legend = year_on_year_energy_usage[,1], beside = TRUE)
