1880740f2441df087e820c6c31d12f5c46a38d38
Daniel <x15004091@student.ncirl.ir>
2019-04-14 01:39:10 +0100

    Weather Data SQLite DB

--- Weather/clean_weather_data.R
+++ Weather/clean_weather_data.R
@@ -10,12 +10,13 @@ weatherDB <- dbConnect(RSQLite::SQLite(), "weather_data.sqlite")
 load_weather_data <- function(station, selected_columns=c()) {
     weather_data = read.csv(paste("raw_data/" ,station, "-daily.csv",  sep = ""), header = TRUE)
     weather_data$station = station
-    weather_data
     stripped_weather_data = weather_data[selected_columns]
+    stripped_weather_data
     stripped_weather_data$date = as.Date(stripped_weather_data$date, format="%d-%b-%Y")
     stripped_weather_data$year = as.integer(year(stripped_weather_data$date))
     stripped_weather_data$month = as.integer(month(stripped_weather_data$date))
     stripped_weather_data$day = as.integer(day(stripped_weather_data$date))
+    stripped_weather_data$datestring = as.character(stripped_weather_data$date)
     dbWriteTable(weatherDB, "irish_weather_stations", stripped_weather_data, append = TRUE)
 }
 
@@ -26,7 +27,7 @@ file_names <- c(
     "mount-dillon",
     "roches-point"
 )
-selected_columns <- c("date", "maxtp", "mintp", "rain", "wdsp", "evap", "soil")
+selected_columns <- c("date", "maxtp", "mintp", "rain", "wdsp", "evap", "soil", "station")
 
 for (station in file_names) {
     load_weather_data(station, selected_columns)
