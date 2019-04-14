57a848cd2c31f2711534ccb1f4c559fbee811e94
Daniel <x15004091@student.ncirl.ir>
2019-04-14 15:25:14 +0100

    Accuracy Value Function

--- Weather/weather_groupings.R
+++ Weather/weather_groupings.R
@@ -3,3 +3,11 @@
 # Created by: daniel
 # Created on: 14/04/2019
 
+source("../functions.R")
+
+weather_data <- load.weatherdb(5)
+View(weather_data)
+colnames(weather_data)
+
+weather_monthly_average_temp <- summarize(group_by(weather_data, year, month), mean_temp = round(mean(temp, na.rm = TRUE), 2))
+View(weather_monthly_average_temp)
--- functions.R
+++ functions.R
@@ -24,3 +24,17 @@ load.energydb <- function(){
     con <- dbConnect(RSQLite::SQLite(), "energy_data.sqlite")
     dbReadTable(con, "irish_energy_data")
 }
+
+# Loads Weather Data, optional parametre to specify the weather station accuracy value[1-5] set to 0 for all data
+load.weatherdb <- function(accuracy = 0) {
+    con <- dbConnect(RSQLite::SQLite(), "weather_data.sqlite")
+    if (accuracy == 0) {
+        dbReadTable(con, "irish_average_weather")
+    } else {
+        query_result <- dbSendQuery(con,
+            paste("SELECT * FROM irish_average_weather WHERE accuracy >= ", accuracy, sep="")
+        )
+        dbFetch(query_result)
+    }
+
+}
