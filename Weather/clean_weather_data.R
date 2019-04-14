cdb424272649015b7e0c1a7396d82c23e2676f86
Daniel <x15004091@student.ncirl.ir>
2019-04-14 01:42:53 +0100

    Comments

--- Weather/clean_weather_data.R
+++ Weather/clean_weather_data.R
@@ -1,25 +1,35 @@
-# Title     : TODO
-# Objective : TODO
+# Title     : Clean Weather Data
+# Objective : To format Weather data from multiple csv files and insert into SQLite Database
 # Created by: daniel
 # Created on: 14/04/2019
 
+# Load core functions
 source("../functions.R")
 
+# Create connection to weather database
 weatherDB <- dbConnect(RSQLite::SQLite(), "weather_data.sqlite")
 
+# Function will create and format daily station weather from csv file
 load_weather_data <- function(station, selected_columns=c()) {
+    # Read CSV File
     weather_data = read.csv(paste("raw_data/" ,station, "-daily.csv",  sep = ""), header = TRUE)
+    # Assign Station name
     weather_data$station = station
+    # Select specific columns
     stripped_weather_data = weather_data[selected_columns]
-    stripped_weather_data
+    # Format date as Date Object
     stripped_weather_data$date = as.Date(stripped_weather_data$date, format="%d-%b-%Y")
+    # Assign date info to own columns
     stripped_weather_data$year = as.integer(year(stripped_weather_data$date))
     stripped_weather_data$month = as.integer(month(stripped_weather_data$date))
     stripped_weather_data$day = as.integer(day(stripped_weather_data$date))
+    # Create a string of date
     stripped_weather_data$datestring = as.character(stripped_weather_data$date)
+    # Append to Weather DB table
     dbWriteTable(weatherDB, "irish_weather_stations", stripped_weather_data, append = TRUE)
 }
 
+# List of file names for data
 file_names <- c(
     "mace-head",
     "dublin-airport",
@@ -27,8 +37,10 @@ file_names <- c(
     "mount-dillon",
     "roches-point"
 )
+# Columns for database
 selected_columns <- c("date", "maxtp", "mintp", "rain", "wdsp", "evap", "soil", "station")
 
+# Look through csv file and add to DB
 for (station in file_names) {
     load_weather_data(station, selected_columns)
 }
