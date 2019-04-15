4c4de706e9c10c432f534c30c0be4532accd3e00
Daniel <x15004091@student.ncirl.ir>
2019-04-15 20:15:50 +0100

    Organised Weather Data

--- Weather/clean_weather_data.R
+++ Weather/clean_weather_data.R
@@ -4,7 +4,8 @@
 # Created on: 14/04/2019
 
 # Load core functions and weather DB
-source("./weather_functions.R")
+source("../functions.R")
+weatherDB <- dbConnect(RSQLite::SQLite(), "weather_data.sqlite")
 
 # Function will create and format daily station weather from csv file
 load_weather_data <- function(station, selected_columns=c()) {
@@ -41,3 +42,38 @@ selected_columns <- c("date", "maxtp", "mintp", "rain", "wdsp", "evap", "soil", 
 for (station in file_names) {
     load_weather_data(station, selected_columns)
 }
+
+query_result <- dbSendQuery(weatherDB,
+"SELECT
+       station,
+       date,
+       year,
+       month,
+       day,
+       datestring,
+       ROUND((maxtp + mintp) / 2, 2)  AS avgtemp, -- Convert for Average Temperature
+       ROUND(wdsp * 1.852) AS wind_kmh, -- Convert Windspeed Knot to KMH
+       rain,
+       evap,
+       soil
+FROM irish_weather_stations;")
+# Fetch data
+weather_station_data <- dbFetch(query_result)
+# print head for validation
+head(weather_station_data)
+
+# Combine weather station data by year month and day and average the values
+weather_avg <- summarize(group_by(weather_station_data, year, month, day, datestring),
+temp = round(mean(avgtemp, na.rm = TRUE), 2),
+wind_kmh = round(mean(wind_kmh, na.rm = TRUE), 2),
+rain = round(mean(rain, na.rm = TRUE), 2),
+evap = round(mean(evap, na.rm = TRUE), 2),
+soil = round(mean(soil, na.rm = TRUE), 2),
+accuracy = n() # Integer value stating number of stations that resulted in value
+)
+head(weather_avg)
+tail(weather_avg)
+
+# Write Data to SQL Database table
+dbWriteTable(weatherDB, "irish_average_weather", weather_avg)
+
